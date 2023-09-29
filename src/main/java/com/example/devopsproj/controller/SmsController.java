aaf706cdbec6615d92f65a9d2fccd918080c347f
sweda133 <you@example.com>
2023-09-29 18:42:44 +0530

    refactored sms controller and service

--- src/main/java/com/example/devopsproj/controller/SmsController.java
+++ src/main/java/com/example/devopsproj/controller/SmsController.java
@@ -23,46 +23,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
+import static java.lang.System.out;
+
 @RequestMapping("/api/v1/OTP")
 @RestController
 @RequiredArgsConstructor
 public class SmsController {
 
-    private final SmsService service;
+    private final SmsService smsService;
     private final IUserService userservice;
     private final SimpMessagingTemplate webSocket;
     private static final String TOPIC_DESTINATION = "/lesson/sms";
     private static final String SECRET_KEY = System.getenv("SMS_SECRET_KEY");
 
 
-    // Endpoint to send an SMS.
     @PostMapping("/send")
-    public ResponseEntity<String> smsSubmit(@RequestBody SmsPojo sms) {
-        try {
-            // Call the service to send the SMS.
-            service.send(sms);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<String> smsSubmit(@RequestBody SmsPojo sms){
+        try{
+            out.println("try");
+            smsService.send(sms);
+        }catch (Exception e){
+            return new ResponseEntity<>("something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        // Notify via WebSocket that the SMS has been sent.
-        webSocket.convertAndSend(TOPIC_DESTINATION, getTimeStamp() + ": SMS has been sent " + sms.getPhoneNumber());
-        return new ResponseEntity<>("OTP sent", HttpStatus.OK);
+        webSocket.convertAndSend(TOPIC_DESTINATION,smsService.getTimeStamp()+":SMS has been sent "+sms.getPhoneNumber());
+        return new ResponseEntity<>("otp sent",HttpStatus.OK);
     }
 
-
-    // Endpoint to verify OTP during signup.
-    @PostMapping("/verify")
-    public Boolean verifyOTPsignup(@RequestBody TempOTP sms, HttpServletResponse response) throws Exception {
-
-        if (sms.getOtp() == StoreOTP.getOtp()) {
-            return true;
+    // Endpoint to verify the OTP for sign-up using phone number and OTP
+    @PostMapping("/verifyOTP/signUp")
+    public String verifyOTPSignUp(@RequestBody TempOTP sms,HttpServletResponse response){
+        if(sms.getOtp()== StoreOTP.getOtp()) {
+            return "correct otp";
         } else {
-            return false;
+            return  "not a correct otp";
         }
-    }
-
-    // Helper method to get the current timestamp.
-    private String getTimeStamp() {
-        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
     }
 }
--- src/main/java/com/example/devopsproj/service/implementations/SmsService.java
+++ src/main/java/com/example/devopsproj/service/implementations/SmsService.java
@@ -8,9 +8,17 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
+
+import java.security.SecureRandom;
 import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.mysql.cj.conf.PropertyKey.logger;
 
 @Component
 @Getter
@@ -23,33 +31,45 @@ public class SmsService {
     private final String AUTH_TOKEN = System.getenv("TWILIO_AUTH");
     private final String FROM_NUMBER = System.getenv("TWILIO_TRIAL_NUMBER");
 
-    public void send(SmsPojo sms) throws ParseException{
+
+    private static final Logger logger = LoggerFactory.getLogger(SmsService.class);
+    public void send(SmsPojo sms) {
+        logger.info("Sending SMS");
         Twilio.init(ACCOUNT_SID,AUTH_TOKEN);
-        int min = 100000;
-        int max = 999999;
-        int number  = (int)(Math.random()*(max-min +1)+min);
-        String msg = "Your OTP - "+number+" . Please verify this OTP. Do not share with anyone. Thank you!";
-        Message message = Message.creator(new PhoneNumber(sms.getPhoneNumber()),new PhoneNumber(FROM_NUMBER),msg)
+        logger.info("Twilio initialized");
+
+        SecureRandom secureRandom = new SecureRandom();
+        int number = secureRandom.nextInt(900000) + 100000; // Generates a random 6-digit number
+
+        String msg = "Your OTP - " + number + "   please verify this OTP   a6mge7C9IMY";
+        Message.creator(new PhoneNumber(sms.getPhoneNumber()), new PhoneNumber(FROM_NUMBER), msg)
                 .create();
         StoreOTP.setOtp(number);
         phoneNumber = sms.getPhoneNumber();
+        logger.info("SMS sent to phone number: {}", phoneNumber);
     }
+
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public void resend(SmsPojo resendsms) throws ParseException{
-        Twilio.init(ACCOUNT_SID,AUTH_TOKEN);
-        int min = 100000;
-        int max = 999999;
-        int number  = (int)(Math.random()*(max-min +1)+min);
-        String msg = "Your OTP is - "+number+" . Please enter the OTP. Do not share it with anyone. Thank you!";
-        Message newmessage = Message.creator(new PhoneNumber(resendsms.getPhoneNumber()),new PhoneNumber(FROM_NUMBER),msg)
-                .create();
-        StoreOTP.setOtp(number);
+//    public void resend(SmsPojo resendsms) throws ParseException{
+//        Twilio.init(ACCOUNT_SID,AUTH_TOKEN);
+//        int min = 100000;
+//        int max = 999999;
+//        int number  = (int)(Math.random()*(max-min +1)+min);
+//        String msg = "Your OTP is - "+number+" . Please enter the OTP. Do not share it with anyone. Thank you!";
+//        Message newmessage = Message.creator(new PhoneNumber(resendsms.getPhoneNumber()),new PhoneNumber(FROM_NUMBER),msg)
+//                .create();
+//        StoreOTP.setOtp(number);
+//    }
+
+//    public void recieve (MultiValueMap<String,String> smscallback){
+//    }
+    public String getTimeStamp() {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
     }
 
-    public void recieve (MultiValueMap<String,String> smscallback){
-    }
+
 }
