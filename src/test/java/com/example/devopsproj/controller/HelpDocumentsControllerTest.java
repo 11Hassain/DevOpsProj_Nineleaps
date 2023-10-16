498b22598f27397cd5eb8e2a12ca324cdbd85f4d
dhassain <dudekula.hassain@nineleaps.com>
2023-10-16 12:34:57 +0530

    Nested test cases

--- src/test/java/com/example/devopsproj/controller/HelpDocumentsControllerTest.java
+++ src/test/java/com/example/devopsproj/controller/HelpDocumentsControllerTest.java
@@ -6,6 +6,8 @@ import com.example.devopsproj.model.HelpDocuments;
 import com.example.devopsproj.service.implementations.HelpDocumentsServiceImpl;
 import com.example.devopsproj.service.implementations.JwtServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -38,204 +40,223 @@ class HelpDocumentsControllerTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    // ----- SUCCESS (For VALID TOKEN) -----
+    @Nested
+    class UploadFileTest {
+        @Test
+        @DisplayName("Testing success case with valid token")
+        void testUploadFile_ValidToken_ValidFile() throws IOException {
+            long projectId = 1L;
+            String accessToken = "valid-access-token";
+            MultipartFile projectFile = createMockMultipartFile();
+            String fileExtension = "txt";
 
-    @Test
-    void testUploadFile_ValidToken_ValidFile() throws IOException {
-        long projectId = 1L;
-        String accessToken = "valid-access-token";
-        MultipartFile projectFile = createMockMultipartFile();
-        String fileExtension = "txt";
+            when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
+            when(helpDocumentsService.getFileExtension(projectFile)).thenReturn(fileExtension);
+            when(helpDocumentsService.uploadFiles(projectId, projectFile, fileExtension)).thenReturn(
+                    ResponseEntity.ok("File uploaded successfully")
+            );
 
-        when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
-        when(helpDocumentsService.getFileExtension(projectFile)).thenReturn(fileExtension);
-        when(helpDocumentsService.uploadFiles(projectId, projectFile, fileExtension)).thenReturn(
-                ResponseEntity.ok("File uploaded successfully")
-        );
+            ResponseEntity<Object> response = helpDocumentsController.uploadFile(projectId, projectFile, accessToken);
 
-        ResponseEntity<Object> response = helpDocumentsController.uploadFile(projectId, projectFile, accessToken);
+            assertNotNull(response);
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertEquals("File uploaded successfully", response.getBody());
 
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("File uploaded successfully", response.getBody());
+            verify(jwtService).isTokenTrue(accessToken);
+            verify(helpDocumentsService).getFileExtension(projectFile);
+            verify(helpDocumentsService).uploadFiles(projectId, projectFile, fileExtension);
+        }
 
-        verify(jwtService).isTokenTrue(accessToken);
-        verify(helpDocumentsService).getFileExtension(projectFile);
-        verify(helpDocumentsService).uploadFiles(projectId, projectFile, fileExtension);
+        @Test
+        @DisplayName("Testing invalid parameters case (Bad Request)")
+        void testUploadFile_InvalidParameters() throws IOException {
+            long projectId = 1L;
+            String accessToken = "valid-access-token";
+            MultipartFile projectFile = createMockMultipartFile();
+            String fileExtension = "txt";
+
+            when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
+            when(helpDocumentsService.getFileExtension(projectFile)).thenReturn(fileExtension);
+            when(helpDocumentsService.uploadFiles(projectId, projectFile, fileExtension))
+                    .thenThrow(new IllegalArgumentException("Invalid parameters"));
+
+            ResponseEntity<Object> response = helpDocumentsController.uploadFile(projectId, projectFile, accessToken);
+
+            assertNotNull(response);
+            assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+            assertEquals("Invalid parameters", response.getBody());
+        }
+
+        @Test
+        @DisplayName("Testing failure case with invalid token")
+        void testUploadFile_InvalidToken() throws IOException {
+            long projectId = 1L;
+            MultipartFile projectFile = createMockMultipartFile();
+
+            when(jwtService.isTokenTrue(anyString())).thenReturn(false);
+
+            ResponseEntity<Object> response = helpDocumentsController.uploadFile(projectId, projectFile, "invalid-access-token");
+
+            assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+            assertEquals("Invalid Token", response.getBody());
+        }
     }
 
-    @Test
-    void testUploadFile_InvalidParameters() throws IOException {
-        long projectId = 1L;
-        String accessToken = "valid-access-token";
-        MultipartFile projectFile = createMockMultipartFile();
-        String fileExtension = "txt";
+    @Nested
+    class GetPdfFilesListTest {
+        @Test
+        @DisplayName("Testing success case with valid token")
+        void testGetPdfFilesList_ValidToken_PdfFilesFound() {
+            long projectId = 1L;
+            String accessToken = "valid-access-token";
 
-        when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
-        when(helpDocumentsService.getFileExtension(projectFile)).thenReturn(fileExtension);
-        when(helpDocumentsService.uploadFiles(projectId, projectFile, fileExtension))
-                .thenThrow(new IllegalArgumentException("Invalid parameters"));
+            when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
 
-        ResponseEntity<Object> response = helpDocumentsController.uploadFile(projectId, projectFile, accessToken);
+            List<HelpDocumentsDTO> pdfFilesList = new ArrayList<>();
+            pdfFilesList.add(new HelpDocumentsDTO());
+            when(helpDocumentsService.getAllDocumentsByProjectId(projectId)).thenReturn(pdfFilesList);
 
-        assertNotNull(response);
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        assertEquals("Invalid parameters", response.getBody());
+            ResponseEntity<Object> response = helpDocumentsController.getPdfFilesList(projectId, accessToken);
+
+            assertNotNull(response);
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertEquals(pdfFilesList, response.getBody());
+        }
+
+        @Test
+        @DisplayName("Testing files not found case")
+        void testGetPdfFilesList_ValidToken_NoPdfFilesFound() {
+            long projectId = 1L;
+            String accessToken = "valid-access-token";
+
+            when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
+
+            when(helpDocumentsService.getAllDocumentsByProjectId(projectId))
+                    .thenThrow(new NotFoundException("No PDF files found"));
+
+            ResponseEntity<Object> response = helpDocumentsController.getPdfFilesList(projectId, accessToken);
+
+            assertNotNull(response);
+            assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+            assertEquals("No PDF files found", response.getBody());
+        }
+
+        @Test
+        @DisplayName("Testing failure case with invalid token")
+        void testGetPdfFilesList_InvalidToken(){
+            long projectId = 1L;
+
+            when(jwtService.isTokenTrue(anyString())).thenReturn(false);
+
+            ResponseEntity<Object> response = helpDocumentsController.getPdfFilesList(projectId,"invalid-access-token");
+
+            assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+            assertEquals("Invalid Token", response.getBody());
+        }
     }
 
-    @Test
-    void testGetPdfFilesList_ValidToken_PdfFilesFound() {
-        long projectId = 1L;
-        String accessToken = "valid-access-token";
+    @Nested
+    class DownloadPdfFileTest {
+        @Test
+        @DisplayName("Testing success case with valid token")
+        void testDownloadPdfFile_ValidToken_PdfFileFound() {
+            String fileName = "example.pdf";
+            String accessToken = "valid-access-token";
 
-        when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
+            when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
 
-        List<HelpDocumentsDTO> pdfFilesList = new ArrayList<>();
-        pdfFilesList.add(new HelpDocumentsDTO());
-        when(helpDocumentsService.getAllDocumentsByProjectId(projectId)).thenReturn(pdfFilesList);
+            HelpDocuments pdfFile = new HelpDocuments();
+            pdfFile.setFileName(fileName);
+            pdfFile.setData(new byte[]{ });
+            when(helpDocumentsService.getPdfFile(fileName)).thenReturn(pdfFile);
 
-        ResponseEntity<Object> response = helpDocumentsController.getPdfFilesList(projectId, accessToken);
+            ResponseEntity<Object> response = helpDocumentsController.downloadPdfFile(fileName, accessToken);
 
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(pdfFilesList, response.getBody());
+            assertNotNull(response);
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertNotNull(response.getBody());
+        }
+
+        @Test
+        @DisplayName("Testing file not found case")
+        void testDownloadPdfFile_ValidToken_PdfFileNotFound() {
+            String fileName = "non-existent.pdf";
+            String accessToken = "valid-access-token";
+
+            when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
+
+            when(helpDocumentsService.getPdfFile(fileName)).thenReturn(null);
+
+            ResponseEntity<Object> response = helpDocumentsController.downloadPdfFile(fileName, accessToken);
+
+            assertNotNull(response);
+            assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        }
+
+        @Test
+        @DisplayName("Testing failure case with invalid token")
+        void testDownloadPdfFile_InvalidToken() {
+            String fileName = "Project.txt";
+
+            when(jwtService.isTokenTrue(anyString())).thenReturn(false);
+
+            ResponseEntity<Object> response = helpDocumentsController.downloadPdfFile(fileName,"invalid-access-token");
+
+            assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+            assertEquals("Invalid Token", response.getBody());
+        }
     }
 
-    @Test
-    void testGetPdfFilesList_ValidToken_NoPdfFilesFound() {
-        long projectId = 1L;
-        String accessToken = "valid-access-token";
+    @Nested
+    class DeleteFileTest {
+        @Test
+        @DisplayName("Testing success case with valid token")
+        void testDeleteFile_ValidToken_DocumentFoundAndDeleted() {
+            Long fileId = 1L;
+            String accessToken = "valid-access-token";
 
-        when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
+            when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
 
-        when(helpDocumentsService.getAllDocumentsByProjectId(projectId))
-                .thenThrow(new NotFoundException("No PDF files found"));
+            HelpDocumentsDTO documentDTO = new HelpDocumentsDTO();
+            when(helpDocumentsService.getDocumentById(fileId)).thenReturn(Optional.of(documentDTO));
 
-        ResponseEntity<Object> response = helpDocumentsController.getPdfFilesList(projectId, accessToken);
+            doNothing().when(helpDocumentsService).deleteDocument(fileId);
 
-        assertNotNull(response);
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-        assertEquals("No PDF files found", response.getBody());
-    }
+            ResponseEntity<String> response = helpDocumentsController.deleteFile(fileId, accessToken);
 
-    @Test
-    void testDownloadPdfFile_ValidToken_PdfFileFound() {
-        String fileName = "example.pdf";
-        String accessToken = "valid-access-token";
+            assertNotNull(response);
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertEquals("Document deleted successfully", response.getBody());
+        }
 
-        when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
+        @Test
+        @DisplayName("Testing document not found case")
+        void testDeleteFile_ValidToken_DocumentNotFound() {
+            Long fileId = 1L;
+            String accessToken = "valid-access-token";
 
-        HelpDocuments pdfFile = new HelpDocuments();
-        pdfFile.setFileName(fileName);
-        pdfFile.setData(new byte[]{ });
-        when(helpDocumentsService.getPdfFile(fileName)).thenReturn(pdfFile);
+            when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
+            when(helpDocumentsService.getDocumentById(fileId)).thenReturn(Optional.empty());
 
-        ResponseEntity<Object> response = helpDocumentsController.downloadPdfFile(fileName, accessToken);
+            ResponseEntity<String> response = helpDocumentsController.deleteFile(fileId, accessToken);
 
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertNotNull(response.getBody());
-    }
+            assertNotNull(response);
+            assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+            assertEquals("Document not found", response.getBody());
+        }
 
-    @Test
-    void testDownloadPdfFile_ValidToken_PdfFileNotFound() {
-        String fileName = "non-existent.pdf";
-        String accessToken = "valid-access-token";
+        @Test
+        @DisplayName("Testing failure case with invalid token")
+        void testDeleteFile_InvalidToken() {
+            Long fileId = 1L;
 
-        when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
+            when(jwtService.isTokenTrue(anyString())).thenReturn(false);
 
-        when(helpDocumentsService.getPdfFile(fileName)).thenReturn(null);
+            ResponseEntity<String> response = helpDocumentsController.deleteFile(fileId,"invalid-access-token");
 
-        ResponseEntity<Object> response = helpDocumentsController.downloadPdfFile(fileName, accessToken);
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-    }
-
-    @Test
-    void testDeleteFile_ValidToken_DocumentFoundAndDeleted() {
-        Long fileId = 1L;
-        String accessToken = "valid-access-token";
-
-        when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
-
-        HelpDocumentsDTO documentDTO = new HelpDocumentsDTO();
-        when(helpDocumentsService.getDocumentById(fileId)).thenReturn(Optional.of(documentDTO));
-
-        doNothing().when(helpDocumentsService).deleteDocument(fileId);
-
-        ResponseEntity<String> response = helpDocumentsController.deleteFile(fileId, accessToken);
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("Document deleted successfully", response.getBody());
-    }
-
-    @Test
-    void testDeleteFile_ValidToken_DocumentNotFound() {
-        Long fileId = 1L;
-        String accessToken = "valid-access-token";
-
-        when(jwtService.isTokenTrue(accessToken)).thenReturn(true);
-        when(helpDocumentsService.getDocumentById(fileId)).thenReturn(Optional.empty());
-
-        ResponseEntity<String> response = helpDocumentsController.deleteFile(fileId, accessToken);
-
-        assertNotNull(response);
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-        assertEquals("Document not found", response.getBody());
-    }
-
-
-    // ----- FAILURE (For INVALID TOKEN) -----
-
-    @Test
-    void testUploadFile_InvalidToken() throws IOException {
-        long projectId = 1L;
-        MultipartFile projectFile = createMockMultipartFile();
-
-        when(jwtService.isTokenTrue(anyString())).thenReturn(false);
-
-        ResponseEntity<Object> response = helpDocumentsController.uploadFile(projectId, projectFile, "invalid-access-token");
-
-        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-        assertEquals("Invalid Token", response.getBody());
-    }
-
-    @Test
-    void testGetPdfFilesList_InvalidToken(){
-        long projectId = 1L;
-
-        when(jwtService.isTokenTrue(anyString())).thenReturn(false);
-
-        ResponseEntity<Object> response = helpDocumentsController.getPdfFilesList(projectId,"invalid-access-token");
-
-        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-        assertEquals("Invalid Token", response.getBody());
-    }
-
-    @Test
-    void testDownloadPdfFile_InvalidToken() {
-        String fileName = "Project.txt";
-
-        when(jwtService.isTokenTrue(anyString())).thenReturn(false);
-
-        ResponseEntity<Object> response = helpDocumentsController.downloadPdfFile(fileName,"invalid-access-token");
-
-        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-        assertEquals("Invalid Token", response.getBody());
-    }
-
-    @Test
-    void testDeleteFile_InvalidToken() {
-        Long fileId = 1L;
-
-        when(jwtService.isTokenTrue(anyString())).thenReturn(false);
-
-        ResponseEntity<String> response = helpDocumentsController.deleteFile(fileId,"invalid-access-token");
-
-        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-        assertEquals("Invalid Token", response.getBody());
+            assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+            assertEquals("Invalid Token", response.getBody());
+        }
     }
 
     // Utility method to create a mock MultipartFile for testing
